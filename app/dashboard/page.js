d0d0c8d2b933fb87a84ff68ddea522faea610896
pankaj ram pawar <pankajpawars123@gmail.com>
2025-02-15 06:48:25 +0530

    udpated lucide react

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,3 @@
-import EventKpi from "../components/event/eventKpi";
-import EventListing from "../components/event/eventListing";
-
 export default function Dashboard() {
     return (
         <div className="flex flex-col w-full gap-4 p-[5%]">       </div>
