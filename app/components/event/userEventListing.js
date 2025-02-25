cd03c7f006998c5418d35074b15354ac82af0d75
pankaj ram pawar <pankajpawars123@gmail.com>
2025-02-25 07:10:42 +0530

    aligned users list

--- app/components/event/userEventListing.js
+++ app/components/event/userEventListing.js
@@ -53,7 +53,7 @@ export default function UserListing({ eventId, usersList }) {
             {/* Header */}
             <div>
                 <div className="flex justify-between items-center mb-6">
-                    <h1 className="text-2xl font-bold">User Listing</h1>
+                    <h1 className="text-2xl font-bold">Users</h1>
                     <div className="flex space-x-4">
                         <input
                             type="text"
@@ -101,8 +101,8 @@ export default function UserListing({ eventId, usersList }) {
                                     </td>
                                     <td className="px-6 py-4">{formatDate(user.dateOfBirth)}</td>
                                     <td className="px-6 py-4">{user.contactNumber}</td>
-                                    <td className="px-6 py-4"><span className={` text-sm p-1 rounded-sm ${user.charmChartFilledOut ? "bg-green-200 text-green-950" : "bg-red-200 text-red-950"}`}>{user.charmChartFilledOut ? "Completed" : "Pending"}</span></td>
-                                    <td className="px-6 py-4 flex itmes-end">
+                                    <td className="px-6 py-4"><span className={` text-sm p-1 rounded-md ${user.charmChartFilledOut ? "bg-green-200 text-green-950" : "bg-red-200 text-red-950"}`}>{user.charmChartFilledOut ? "Completed" : "Pending"}</span></td>
+                                    <td className="px-6 pt-3">
                                         <button onClick={() => router.push(`/dashboard/events/user/${user._id}?data=${encodeURIComponent(JSON.stringify(user))}`)} className="text-slate-400 hover:text-purple-700"><Eye /></button>
                                     </td>
                                 </motion.tr>
