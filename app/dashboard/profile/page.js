04d8a5d048f599209a2b82ab1a8b4e084d3f8faf
pankaj ram pawar <pankajpawars123@gmail.com>
2025-02-25 18:23:36 +0530

    updated editable details for doctorl

--- app/dashboard/profile/page.js
+++ app/dashboard/profile/page.js
@@ -188,6 +188,7 @@ export default function EditDoctor() {
                         type="email"
                         name="email"
                         value={formData.email}
+                        disabled={true}
                         onChange={handleChange}
                         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                         required
@@ -228,6 +229,7 @@ export default function EditDoctor() {
                     <label className="block text-sm font-medium text-gray-700">Role</label>
                     <select
                         name="role"
+                        disabled={true}
                         value={formData.role}
                         onChange={handleChange}
                         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
