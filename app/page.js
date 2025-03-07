88798de60b17d5f2ffff5dd38b9cdd7a341d7ca4
pankaj ram pawar <pankajpawars123@gmail.com>
2025-03-07 22:39:37 +0530

    added routing to home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
 'use client'
+import { useAuth } from "@/context/authContext";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
-
-
+  const router = useRouter();
+  const { user, loading: authLoading, logout } = useAuth();
+  const isAdmin = user?.role === 'admin';
+  const isDoctor = user?.role === 'doctor';
+  useEffect(() => {
+    if (!authLoading) {
+      if (isAdmin || isDoctor) {
+        router.push('/dashboard')
+      }
+    }
+  }, [])
 
   return (
     <div className="">
